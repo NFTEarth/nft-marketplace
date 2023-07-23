7b2ea572b71fcfb31a681cd4c32e510209c74895
Weston Nelson <dogooddowell@protonmail.com>
2023-07-22 22:49:30 -0500

    Update Head.tsx
    Signed-off-by: Weston Nelson <dogooddowell@protonmail.com>

--- components/Head.tsx
+++ components/Head.tsx
@@ -15,8 +15,8 @@ type Props = {
  */
 export const Head: FC<Props> = ({
   ogImage = 'https://explorer.reservoir.tools/og-image.png',
-  title = 'Reservoir | Multi-Chain NFT Explorer',
-  description = 'Reservoir Multi-Chain NFT Explorer is an open source NFT explorer built with Reservoir.',
+  title = 'NFTEarth | Omnichain NFT Hub',
+  description = 'NFTEarth is an open-source NFT hub built with Reservoir.',
 }) => {
   return (
     <NextHead>
@@ -31,7 +31,7 @@ export const Head: FC<Props> = ({
 
       {/* Twitter */}
       <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:site" content="@reservoir0x" />
+      <meta name="twitter:site" content="@NFTEarth_L2" />
       <meta name="twitter:title" content={title} />
       <meta name="twitter:description" content={description} />
       <meta name="twitter:image" content={ogImage} />
@@ -46,7 +46,7 @@ export const Head: FC<Props> = ({
       <meta property="og:image:type" content="image/png" />
       <meta property="og:image:width" content="1200" />
       <meta property="og:image:height" content="630" />
-      <meta property="og:image:alt" content="Reservoir NFT Explorer Banner" />
+      <meta property="og:image:alt" content="NFTEarth" />
     </NextHead>
   )
 }
