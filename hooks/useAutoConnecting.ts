30b4621c7fa46b4fd29bc789cfc6b948b8ce719f
Weston <dogooddowell@protonmail.com>
2023-11-12 18:25:53 -0600

    troubleshooting hooks

--- hooks/useAutoConnecting.ts
+++
@@ -1,9 +0,0 @@
-import useStore from "../hooks/useStore";
-
-const useAutoConnecting = () => {
-  const autoConnecting = useStore((state) => state.autoConnecting);
-  const setAutoConnecting = useStore((state) => state.setAutoConnecting);
-  return { autoConnecting, setAutoConnecting };
-};
-
-export default useAutoConnecting;
--- utils/constants.ts
+++
@@ -1,9 +0,0 @@
-import {Map} from "../types/map";
-
-export const NFTE_TOKEN_ADDRESS = '0x51b902f19a56f0c8e409a34a215ad2673edf3284';
-export const WETH_TOKEN_ADDRESS = '0x82aF49447D8a07e3bd95BD0d56f35241523fBab1';
-export const CHAIN_ID = 42161
-
-export const nfteContractAddresses: Map = {
-  42161: NFTE_TOKEN_ADDRESS,
-};
