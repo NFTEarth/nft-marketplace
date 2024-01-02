1053b00c7696075adcad7bfefd708a8beefaa90f
Weston <dogooddowell@protonmail.com>
2024-01-02 12:36:27 -0600

    Update

--- utils/chains.ts
+++ utils/chains.ts
@@ -172,31 +172,31 @@ export type ReservoirChain = Chain & {
 }
 
 export const DefaultChain: ReservoirChain = {
-  ...optimism,
+  ...base,
   // Any url to display the logo of the chain in light mode
-  lightIconUrl: '/icons/optimism-icon-dark.svg',
+  lightIconUrl: '/icons/base-icon-dark.svg',
   // Any url to display the logo of the chain in dark mode
-  darkIconUrl: '/icons/optimism-icon-light.svg',
+  darkIconUrl: '/icons/base-icon-light.svg',
   // The base url of the reservoir api, this is used in the app when
   // directly interacting with the reservoir indexer servers (in the api proxy for example)
   // or when prefetching server side rendered data
-  reservoirBaseUrl: 'https://api-optimism.reservoir.tools',
+  reservoirBaseUrl: 'https://api-base.reservoir.tools',
   // Used on the client side portions of the marketplace that need an api key added
   // Prevents the api key from being leaked in the clientside requests
   // If you'd like to disable proxying you can just change the proxyApi to the reservoirBaseUrl
   // Doing so will omit the api key unless further changes are made
-  proxyApi: '/api/reservoir/optimism',
+  proxyApi: '/api/reservoir/base',
   // A prefix used in the asset specific routes on the app (tokens/collections)
-  routePrefix: 'optimism',
+  routePrefix: 'base',
   // Reservoir API key which you can generate at https://reservoir.tools/
   // This is a protected key and displays as 'undefined' on the browser
   // DO NOT add NEXT_PUBLIC to the key or you'll risk leaking it on the browser
   apiKey: process.env.RESERVOIR_API_KEY,
   // Coingecko id, used to convert the chain's native prices to usd. Can be found here:
   // https://www.coingecko.com/en/api/documentation#operations-coins-get_coins_list
-  coingeckoId: 'optimism',
-  collectionSetId: process.env.NEXT_PUBLIC_OPTIMISM_COLLECTION_SET_ID,
-  community: process.env.NEXT_PUBLIC_OPTIMISM_COMMUNITY,
+  coingeckoId: 'base',
+  collectionSetId: process.env.NEXT_PUBLIC_BASE_COLLECTION_SET_ID,
+  community: process.env.NEXT_PUBLIC_BASE_COMMUNITY,
 }
 
 type NFTBridge = {
