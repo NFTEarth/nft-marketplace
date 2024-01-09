570d095ed3781d7257389948d841d2162c66de7e
Weston <dogooddowell@protonmail.com>
2024-01-08 21:02:24 -0600

    chore: update APR

--- hooks/useAPR2.ts
+++ hooks/useAPR2.ts
@@ -2,6 +2,6 @@ import { BalancerSDK, BalancerSdkConfig, Network } from '@balancer-labs/sdk';
 
 const config: BalancerSdkConfig = {
   network: Network.BASE,
-  rpcUrl: `https://mainnet.infura.io/v3/${process.env.NEXT_PUBLIC_INFURA_ID}`,
+  rpcUrl: `https://base.infura.io/v3/${process.env.NEXT_PUBLIC_INFURA_ID}`,
 };
 const balancer = new BalancerSDK(config);
